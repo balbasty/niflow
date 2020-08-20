ba4e02118d692b51fff28171fc6d4718f71f086e
Yaël Balbastre <yael.balbastre@gmail.com>
2020-08-20 12:58:34 +0100

    Fix setup script

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 from setuptools import setup
 
+# Note
+# ----
+# Git dependencies are not handled the same way by setuptools and pip:
+# - setuptools browses repositories listed in `dependency_links`
+# - pip uses the syntax 'repo @ url' in `install_requires`
+# I am using both, because I want users to be able to choose between
+# setuptools and pip.
+
 setup(
     name='niflow',
     version='0.1.0',
@@ -10,7 +18,7 @@ setup(
     author_email='yael.balbastre@gmail.com',
     description='Neuroimaging in tensorflow',
     python_requires='>=3.5',
-    install_requires=['unik==0.1.0',
+    install_requires=['unik @ git+https://github.com/balbasty/unik#egg=unik-0.1.0',
                       'nibabel'],
-    dependency_links=['git+https://github.com/balbasty/unik@master#egg=unik-0.1.0'],
+    dependency_links=['git+https://github.com/balbasty/unik#egg=unik-0.1.0'],
 )
